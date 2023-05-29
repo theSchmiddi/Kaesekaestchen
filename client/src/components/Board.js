3ef0d5daa1fd4c721e55685d648de596595494b4
theSchmiddi <marco.schmidt03@gmail.com>
2023-05-29 22:28:37 +0200

    do not know what I do

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Cell from "./Cell";
-import { socket, roomId } from "./StartScreen";
 import "./board.css";
 
 function Board() {
   const [board, setBoard] = useState(
-    Array.from({ length: 7 }, () =>
-      Array.from({ length: 7 }, () => ({
-        horizontal: false,
-        vertical: false,
+    Array.from({ length: 9 }, () =>
+      Array.from({ length: 9 }, () => ({
+        top: false,
+        right: false,
+        bottom: false,
+        left: false,
         owner: null,
       }))
     )
@@ -16,10 +16,7 @@ function Board() {
   const [currentPlayer, setCurrentPlayer] = useState(1);
 
   useEffect(() => {
-    socket.on("updateBoard", ({ newBoard, nextPlayer }) => {
-      setBoard(newBoard);
-      setCurrentPlayer(nextPlayer);
-    });
+    // Hier können Sie den Code für die Socket-Verbindung hinzufügen
   }, []);
 
   const getNextPlayer = () => {
@@ -36,58 +33,58 @@ function Board() {
           )
         : rowCells
     );
-    socket.emit("makeMove", {
-      roomId,
-      newBoard,
-      nextPlayer: getNextPlayer(),
-    });
+    setBoard(newBoard);
+    setCurrentPlayer(getNextPlayer());
   };
-
-  const renderCell = (row, col, edge) => {
+  
+  const renderCell = (row, col) => {
     const cell = board[row][col];
     const owner = cell.owner;
-    const isDisabled = owner !== null;
-    const color = owner === 1 ? "red" : "blue";
-    const className = `board-cell-${edge}${
-      owner !== null ? ` board-cell-${color}` : ""
-    }${isDisabled ? " board-cell-disabled" : ""}`;
-    const handleClick = () => {
+    const top = row === 0 || board[row - 1][col].bottom;
+    const right = col === 8 || board[row][col + 1].left;
+    const bottom = row === 8 || board[row + 1][col].top;
+    const left = col === 0 || board[row][col - 1].right;
+    const topClassName = `board-cell-top${top ? " board-cell-top-set" : ""}${
+      owner === 1 ? " board-cell-top-player1" : owner === 2 ? " board-cell-top-player2" : ""
+    }`;
+    const rightClassName = `board-cell-right${
+      right ? " board-cell-right-set" : ""
+    }${owner === 1 ? " board-cell-right-player1" : owner === 2 ? " board-cell-right-player2" : ""}`;
+    const bottomClassName = `board-cell-bottom${
+      bottom ? " board-cell-bottom-set" : ""
+    }${owner === 1 ? " board-cell-bottom-player1" : owner === 2 ? " board-cell-bottom-player2" : ""}`;
+    const leftClassName = `board-cell-left${left ? " board-cell-left-set" : ""}${
+      owner === 1 ? " board-cell-left-player1" : owner === 2 ? " board-cell-left-player2" : ""
+    }`;
+    const handleClick = (edge) => () => {
       handleMakeMove({ row, col, edge });
     };
     return (
-      <Cell
-        key={`${row}-${col}-${edge}`}
-        className={className}
-        onClick={handleClick}
-      />
+      <div key={`${row}-${col}`} className="board-cell">
+        <div className={topClassName}></div>
+        <div className={rightClassName}></div>
+        <div className={bottomClassName}></div>
+        <div className={leftClassName}></div>
+        {row < 8 && col < 8 && <div className="board-cell-square"></div>}
+        {row === 8 && col < 8 && <div className="board-cell-square board-cell-bottom"></div>}
+        {col === 8 && row < 8 && <div className="board-cell-square board-cell-right"></div>}
+      </div>
     );
   };
 
-  const renderRow = (row, rowIndex) => {
+  const renderRow = (rowIndex) => {
     const cells = [];
-    for (let colIndex = 0; colIndex < 6; colIndex++) {
-      cells.push(
-        <React.Fragment key={`${rowIndex}-${colIndex}`}>
-          {renderCell(rowIndex, colIndex, "horizontal")}
-          {renderCell(rowIndex, colIndex, "vertical")}
-        </React.Fragment>
-      );
+    for (let colIndex = 0; colIndex < 9; colIndex++) {
+      cells.push(renderCell(rowIndex, colIndex));
     }
-    cells.push(renderCell(rowIndex, 6, "horizontal"));
-    return (
-      <React.Fragment key={rowIndex}>
-        <div className="board-row">{cells}</div>
-        <div className="board-row-spacer"></div>
-      </React.Fragment>
-    );
+    return <div key={rowIndex} className="board-row">{cells}</div>;
   };
 
   const renderBoard = () => {
     const rows = [];
-    for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
-      rows.push(renderRow(rowIndex, rowIndex));
+    for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+      rows.push(renderRow(rowIndex));
     }
-    rows.push(renderRow(6, 6));
     return <div className="board">{rows}</div>;
   };
 
