273d1d56e2f8413509ca4c2fb4e89de5088c6e68
theSchmiddi <marco.schmidt03@gmail.com>
2023-05-30 09:15:10 +0200

    fixed winning condition

--- server/index.js
+++ server/index.js
@@ -134,7 +134,9 @@ io.on("connection", (socket) => {
         edges,
         nextPlayer: game.currentPlayer,
       });
-      if (score === 0 && edges.every((edge) => edge)) {
+      console.log(edges)
+      if (!squares.includes(0)) {
+        console.log("Fertig")
         const winner = squares.reduce(
           (acc, square) => {
             acc[square - 1]++;
